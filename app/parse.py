7ef316c53ffc9a04f32c4166ec37db2e0bedcebe
Vadym Tiron <tironvadim1@gmail.com>
2024-07-07 19:27:22 +0300

    fix import

--- app/parse.py
+++ app/parse.py
@@ -1,10 +1,9 @@
-from app import output_csv_path
+from app.output_csv_path import get_quotes, write_quotes_to_csv
 
 
 def main() -> None:
-    quotes = output_csv_path.get_quotes()
-
-    output_csv_path.write_quotes_to_csv(quotes)
+    quotes = get_quotes()
+    write_quotes_to_csv(quotes)
     print("Quotes have been written")
 
 
